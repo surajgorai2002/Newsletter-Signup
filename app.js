39e2363ccd5d9adf2b06fd8b2a5e5c3985335dc3
surajgorai2002 <surajgorai2002@gmail.com>
2022-08-01 00:00:26 +0530

    change

---
+++ app.js
@@ -0,0 +1,56 @@
+const express=require("express")
+const https=require("https")
+const bodyParser=require("body-parser")
+const app=express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+app.get("/",function(req,res){
+   
+    res.sendFile(__dirname+"/index.html");
+})
+app.post("/",function(req,res){
+    const firstname = req.body.fname;
+   const lastname = req.body.lname;
+    const email = req.body.email;
+    const data = {
+        members:[
+            {
+                email_address: email,
+                status:"subscribed",
+              merge_fields: {
+                FNAME:firstname,
+                LNAME:lastname
+              }
+            }
+        ]
+    }
+    const jsonData=JSON.stringify(data);
+    const url="https://us13.api.mailchimp.com/3.0/lists/84eb25dee6";
+    const options={
+        method:"POST",
+        auth:"suraj:ea85e5e47002c952c8356b03b627c3166-us13"
+    }
+    const request=https.request(url,options,function(response){
+        if(response.statusCode === 200)
+        res.sendFile(__dirname+"/success.html")
+        else
+        res.sendFile(__dirname+"/failure.html")
+        response.on("data",function(data){
+        console.log(JSON.parse(data));
+        })
+    })
+    request.write(jsonData);
+    request.end();
+
+})
+
+app.post("/failure",function(req,res){
+    res.redirect("/");
+})
+app.listen(process.env.PORT || 3000,function(req,res){
+    console.log("started");
+})
+
+
+// API key e85e5e47002c952c8356b03b627c3166-us13
+// id 84eb25dee6
